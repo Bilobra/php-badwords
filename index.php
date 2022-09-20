2b8b28dfe3fc293d0f7a9dad6a20d8ccaf5be261
Bilobra <bra.carrozzo@gmail.com>
2022-09-20 15:09:38 +0200

    add text length

--- index.php
+++ index.php
@@ -9,11 +9,12 @@ $text = 'Il Lonfo non vaterca né gluisce
         È frusco il Lonfo! È pieno di lupigna
         arrafferia malversa e sofolenta!
         Se cionfi ti sbiduglia e ti arrupigna
-        se lugri ti botalla e ti criventa.'
+        se lugri ti botalla e ti criventa.';
 
-
+$length = strlen($text);
 
 ?>
 
 <h1>Il Lonfo</h1>
 <p> <?php echo $text; ?> </p>
+<p> <strong> il paragrafo è composto da <?php echo $length; ?> caratteri. </strong></p>
